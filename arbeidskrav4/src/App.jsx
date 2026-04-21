781c1e7020449a5a44fbf36f524b4163e5e1664e
VTLE249 <vthle@hiof.no>
2026-04-21 10:24:02 +0200

    add student1 component

---
+++ arbeidskrav4/src/App.jsx
@@ -0,0 +1,27 @@
+import student1 from "./components/student1";
+
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>UIN26_1</h1>
+      </header>
+
+      <main>
+        <section>
+          <h2>Gruppemedlemmer</h2>
+
+          <Student1 />
+        </section>
+
+        <section>
+          <h2>Arbeidskrav4</h2>
+          <p> Sanity </p>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ arbeidskrav4/src/components/student1.jsx
@@ -0,0 +1,21 @@
+import "./student1.css";
+
+export default function student1() {
+  return (
+    <article>
+      <img
+        src="https://api.dicebear.com/7.x/bottts/svg?seed=vuthach"
+        alt="avatar av Vu Thach"
+        className="avatar"
+      />
+
+      <h3>Vu Thach</h3>
+
+      <p>
+        <a href="mailto:vthle@hiof.no">vthle@hiof.no</a>
+      </p>
+
+      <p>Bachelor i informasjonssystemer</p>
+    </article>
+  );
+}
